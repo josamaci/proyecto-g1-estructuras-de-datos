ac0d39fa81dbfa9b25ff913e83578617db72a9e1
nicolas <niplinig@espol.edu.ec>
2021-10-29 18:33:37 -0500

    Implementación del TDA Lista Circular Doblemente Enlazada
    Co-Authored-By: Nicolás Andrés Plaza Iñiguez <niplinig@users.noreply.github.com>

---
+++ Grupo_01/src/main/java/TDAs/DoubleNode.java
@@ -0,0 +1,38 @@
+package TDAs;
+
+public class DoubleNode<E> {
+
+    private E content;
+    private DoubleNode<E> next;
+    private DoubleNode<E> previous;
+
+    public DoubleNode(E element) {
+        this.content = element;
+        this.next = this;
+        this.previous = this;
+    }
+
+    public E getContent() {
+        return content;
+    }
+
+    public DoubleNode<E> getNext() {
+        return next;
+    }
+
+    public DoubleNode<E> getPrevious() {
+        return previous;
+    }
+
+    public void setNext(DoubleNode<E> e) {
+        this.next = e;
+    }
+
+    public void setContent(E e) {
+        this.content = e;
+    }
+
+    public void setPrevious(DoubleNode<E> previous) {
+        this.previous = previous;
+    }
+}
