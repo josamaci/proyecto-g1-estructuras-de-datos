966460ad3b58822a5d85dcb8eaef1824f352a490
codeswax <kevvsole@espol.edu.ec>
2021-11-02 00:05:09 -0500

    enlace inicial de pantallas

--- Grupo_01/src/main/java/espol/grupo_01/SettingsFXMLController.java
+++ Grupo_01/src/main/java/espol/grupo_01/SettingsFXMLController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
+import javafx.scene.control.ToggleButton;
 
 public class SettingsFXMLController {
 
@@ -11,9 +12,26 @@ public class SettingsFXMLController {
     private Tab boardTab;
     @FXML
     private Button btBackToTitle;
+    @FXML
+    private ToggleButton btBoard7;
+    @FXML
+    private ToggleButton btBoard9;
+    @FXML
+    private ToggleButton btBoard11;
+    @FXML
+    private ToggleButton btBoard13;
+    @FXML
+    private ToggleButton btBoardRd;
+    @FXML
+    private Button btStartGame;
 
     @FXML
     private void switchToMainMenu() throws IOException {
         App.setRoot("MainMenuFXML");
     }
+    @FXML
+    private void switchToGame() throws IOException {
+        App.setRoot("PlayboardFXML");
+    }
+
 }
