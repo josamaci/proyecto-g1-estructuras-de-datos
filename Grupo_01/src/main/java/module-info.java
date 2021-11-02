780e9cffd1a169efb53cebb60c295f9dba3d2d83
josamaci <84406998+josamaci@users.noreply.github.com>
2021-11-01 23:37:30 -0500

    eo

--- Grupo_01/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module espol.grupo_01 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens espol.grupo_01 to javafx.fxml;
-    exports espol.grupo_01;
-}
