db837b8d2c5a02899fe703f0aeb8e820fd8eec24
codeswax <kevvsole@espol.edu.ec>
2021-11-01 22:12:41 -0500

    resolviendo problema de ejecucion

--- Grupo_01/src/main/java/espol/grupo_01/SettingsFXMLController.java
+++ Grupo_01/src/main/java/espol/grupo_01/SettingsFXMLController.java
@@ -2,8 +2,15 @@ package espol.grupo_01;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
 
 public class SettingsFXMLController {
+
+    @FXML
+    private Tab boardTab;
+    @FXML
+    private Button toMainMenuBtn;
 
     @FXML
     private void switchToMainMenu() throws IOException {
