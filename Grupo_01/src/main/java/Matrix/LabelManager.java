3b386553fcbf100cbc62edc8ec3c1abd6a68acc0
pop <pop@pop-os.localdomain>
2021-12-01 18:25:15 -0500

    Más cambios

--- Grupo_01/src/main/java/Matrix/LabelManager.java
+++ Grupo_01/src/main/java/Matrix/LabelManager.java
@@ -5,9 +5,9 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import javafx.event.Event;
 import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
@@ -17,32 +17,40 @@ public class LabelManager {
     public static String selectedWord = "";
     public static Queue<String> selectedLabel = new PriorityQueue<>();
        
-    public static void styleLabel(Label l) {
+    public static void styleLabel(Button l) {
 //        Label l = new Label(Randomer.getRdStr());
         l.setPrefSize(60, 60);
         l.setMaxSize(60, 60);
         l.setAlignment(Pos.CENTER);
-        l.setStyle("-fx-background-color: WHITE; -fx-border-color: BLACK");
+//        l.setStyle("-fx-background-color: WHITE; -fx-border-color: BLACK");
         
-//        l.setOnMouseClicked(new EventHandler<MouseEvent>() {
-//            @Override
-//            public void handle(MouseEvent t) {
-//                
+        l.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+                
 //                Node n = t.getPickResult().getIntersectedNode();
-//                if (n instanceof Label) {
-//                    Label l = (Label) n;
-//                    l.setStyle("-fx-background-color: LIGHTBLUE; -fx-border-color: BLACK");
-//                    selectedWord += l.getText();
-//                    System.out.println(selectedWord);
+//                if (n instanceof Button) {
+//                    Button l = (Button) n;
+                  if (t.isDragDetect()) {
+                      l.setStyle("-fx-background-color: LIGHTBLUE; -fx-border-color: BLACK");
+                      selectedWord += l.getText();
+                      System.out.println(selectedWord);
+                      l.setDisable(true);
+                    }
+
 //                }
-////                System.out.println(t.getPickResult().getIntersectedNode().getTypeSelector());
-//            }
-//        });
-        l.setOnMousePressed((Event t) -> {
-            
-            l.setStyle("-fx-background-color: LIGHTBLUE; -fx-border-color: BLACK");
-            
+//                System.out.println(t.getPickResult().getIntersectedNode().getTypeSelector());
+            }
         });
+//        l.setOnMouseClicked((Event t) -> {
+//            
+//            
+//            l.setDisable(true);
+//            
+//        });
+
+//        l.addEventFilter(MouseEvent.MOUSE_DRAGGED, );
+
     }
     
     public static boolean verifyWord() {
